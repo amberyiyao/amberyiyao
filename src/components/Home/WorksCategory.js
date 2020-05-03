f15a3c5693329e9fde64ce517ae57d14863c2def
zhou0160 <amber.zhou@non.se.com>
2020-05-03 15:49:57 -0400

    changed some layout

--- src/components/Home/WorksCategory.js
+++ src/components/Home/WorksCategory.js
@@ -6,9 +6,8 @@ export default function WorksCategory(props) {
   return (
     <div className="homeWorksCategory">
         <p onClick={() => onChangeCategory(0)} className={categoryId == 0? "homeWorksCategoryItem active" : "homeWorksCategoryItem"}>ALL</p>
-        <p onClick={() => onChangeCategory(1)} className={categoryId == 1? "homeWorksCategoryItem active" : "homeWorksCategoryItem"}>WEBSITE</p>
-        <p onClick={() => onChangeCategory(2)} className={categoryId == 2? "homeWorksCategoryItem active" : "homeWorksCategoryItem"}>MOBILE APP</p>
-        <p onClick={() => onChangeCategory(3)} className={categoryId == 3? "homeWorksCategoryItem active" : "homeWorksCategoryItem"}>DESIGN</p>
+        <p onClick={() => onChangeCategory(1)} className={categoryId == 1? "homeWorksCategoryItem active" : "homeWorksCategoryItem"}>DEVELOPMENT</p>
+        <p onClick={() => onChangeCategory(2)} className={categoryId == 2? "homeWorksCategoryItem active" : "homeWorksCategoryItem"}>DESIGN</p>
     </div>
   );
 }
